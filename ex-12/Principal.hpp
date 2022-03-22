5ddd14c828761cb6f9594d353ab35059d729c3db
Thiago Frois <thiagofrois@outlook.com.br>
2022-03-22 08:51:44 -0300

    Adicionando a solução do exercício 12.

---
+++ ex-12/Principal.hpp
@@ -0,0 +1,15 @@
+#include "Vetor.hpp"
+
+class Principal
+{
+private:
+    Vetor v1, v2;
+public:
+    //Construtora
+    Principal();
+
+    //Destrutora
+    ~Principal();
+
+    void executar();
+};
