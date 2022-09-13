a19f90ba024449c9befe1742f67d8273250cc3c5
Ian Bolfarini <ian.bolfarini@wesee.online>
2022-09-13 16:17:05 -0300

    Adiciona comentários e novos testes

--- grupoDeSlides_03A/ex-16/ex-16_c++/ex-16_final/ex-16_VS/Principal.cpp
+++ grupoDeSlides_03A/ex-16/ex-16_c++/ex-16_final/ex-16_VS/Principal.cpp
@@ -26,14 +26,36 @@ void Principal::setClientesEContas()
 	contas[4] = Conta(5);
 
 	// Relaciona os clientes e contas
+	/* 
+	 * Um cliente pode ser titular de até 3 contas.
+	 * Note que a relação é dada por associação, 
+	 * considerando que a classe cliente recebe 
+	 * o objeto do tipo conta via ponteiro. 
+	 * O qual inclusive pode ser compartilhado 
+	 * entre instâncias da classe cliente.
+	 */
 	clientes[0].criarConta(&contas[0]);
 	clientes[0].criarConta(&contas[1]);
 	clientes[0].criarConta(&contas[2]);
+	
+	/* 
+	 * A conta no índice 4 é vinculada ao cliente,
+	 * neste caso ele não é o titular dessa conta.
+	 * A relação é dada por associação.
+	 */
 	clientes[0].vincularConta(&contas[4]);
 
+	/*
+	* O cliente no índice 1 é titular da conta 
+	* no índice 3, e não titular da conta no índice 0.
+	*/
 	clientes[1].criarConta(&contas[3]);
 	clientes[1].vincularConta(&contas[0]);
 
+	/*
+	* O cliente no índice 2 é titular da conta
+	* no índice 4, e não titular das conta nos índice 1 e 3.
+	*/
 	clientes[2].criarConta(&contas[4]);
 	clientes[2].vincularConta(&contas[1]);
 	clientes[2].vincularConta(&contas[3]);
@@ -55,10 +77,12 @@ void Principal::executar()
 	clientes[0].aplicarRecursos(1, 7200.56);
 	clientes[0].aplicarRecursos(2, 1300.45);
 	clientes[0].aplicarRecursos(3, 180);
-	clientes[1].aplicarRecursos(4, -1000);
+	clientes[1].aplicarRecursos(4, 1000);
+	clientes[1].sacarRecursos(4, 500);
+
 	geraRelatorioClientes();
 
-	clientes[1].aplicarRecursos(4, -600);
+	clientes[1].aplicarRecursos(4, 600);
 
 	geraRelatorioClientes();
 
@@ -70,4 +94,11 @@ void Principal::executar()
 
 	geraRelatorioClientes();
 
+	clientes[1].aplicarRecursos(4, 300);
+
+	geraRelatorioClientes();
+
+	clientes[1].sacarRecursos(4, 200);
+
+	geraRelatorioClientes();
 }
